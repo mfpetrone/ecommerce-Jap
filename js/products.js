f202b1f1f6c45c70e115fe4788bca9b1a003ef1e
mfpetrone <mariaflorenciapetrone@gmail.com>
2021-08-21 14:52:04 -0300

    productosincorporados

--- js/products.js
+++ js/products.js
@@ -1,43 +1,39 @@
-let array = [];
+document.addEventListener("DOMContentLoaded", function (e){
+    
+    let url="https://japdevdep.github.io/ecommerce-api/product/all.json"; 
+        
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                
+                 for(let i=0; i < data.length; i++){
+                        let auto = data[i]
+            
+                        let nombre = auto.name;
+                        let descripcion = auto.description;                        
+                        let moneda = auto.currency;
+                        let costo = auto.cost;
+                        let imagen = auto.imgSrc;
+                        let cantvendido = auto.soldCount;
 
-function showProductoList(array){
-
-    let htmlContentToAppend = "";
-    for(let i = 0; i < array.length; i++){
-        let product = array[i];
-
-        htmlContentToAppend += `
-            <div class="list-group-item list-group-item-action">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ product.name +`</h4>
-                            <small class="text-muted">` + product.currency + " "+ product.cost +`</small>
-                        </div>
-                        <div><p class="mb-1">` + product.description + `</p></div>
-                    </div>
-                </div>
-            </div>
-            `
-        }
-
-        document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
-    }
-
-
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-
-document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if(resultObj.status === "ok"){
-            productoArr = resultObj.data;
-            showProductoList(productoArr);
-        }
-    })  
-
-});
+                        document.getElementById("producto-list-container").innerHTML +=`
+                            <div class="list-group-item list-group-item-action">
+                                        <div class="row">
+                                            <div class="col-3">
+                                                 <img src="` + imagen + `" alt="` + descripcion + `" class="img-thumbnail">
+                                             </div>
+                                             <div class="col">
+                                                 <div class="d-flex w-100 justify-content-between">
+                                                     <h4 class="mb-1">`+ nombre + `</h4>
+                                                     <small class="text-muted">` + moneda + " " + costo + `</small>
+                                                 </div>
+                                                 <div><p class="mb-1">` + descripcion + `</p></div>
+                                                 <div><p class="mb-2">`+"Cantidad vendida: " + cantvendido + `</p small></div>
+                                             </div>
+                                       </div>
+                        `
+                        
+                        
+                 }   
+             });
+})
